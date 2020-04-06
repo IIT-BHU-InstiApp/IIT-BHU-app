c3b0b3cd117433c206d6737e9e9430f05a464a94
Ayush Kumar Shaw <aksayushx@gmail.com>
2020-04-07 02:25:20 +0530

    Updated Council Screen with relative scrolling (#30)

--- lib/pages/council.dart
+++ lib/pages/council.dart
@@ -71,15 +71,16 @@ class _CouncilPageState extends State<CouncilPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: ListView(
-        children: [
-          Container(
-            padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 0.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              //scrollDirection: Axis.vertical,
-              children: <Widget>[
-                councilData == null
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              backgroundColor: Colors.grey[300],
+              floating: true,
+              expandedHeight: MediaQuery.of(context).size.height / 4,
+              flexibleSpace: FlexibleSpaceBar(
+                //title: Text('SnTC'),
+                background: councilData == null
                     ? Container(
                         height: MediaQuery.of(context).size.height / 4,
                         child: Center(
@@ -101,157 +102,181 @@ class _CouncilPageState extends State<CouncilPage> {
                         ),
                         elevation: 2.5,
                       ),
-                space,
-                Container(
-                  color: Colors.white,
-                  //margin: EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Container(
-                        color: Colors.white,
-                        padding: EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            Text(
-                              'Description',
-                              style: headingStyle,
-                            ),
-                            divide,
-                            councilData == null
-                                ? Center(
-                                    child: CircularProgressIndicator(),
-                                  )
-                                : Text(councilData.description,
-                                    style: generalTextStyle),
-                            space,
-                          ],
-                        ),
-                      ),
-                      councilData == null
-                          ? Container(
-                              height: MediaQuery.of(context).size.height / 4,
-                              child: Center(
-                                child: CircularProgressIndicator(),
-                              ),
-                            )
-                          : Container(
-                              color: Colors.grey[300],
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: <Widget>[
-                                  // template(
-                                  //   imageUrl:
-                                  //       councilData.joint_gensec[0].photo_url,
-                                  //   desg: 'joint gensec',
-                                  //   name: councilData.joint_gensec[0].name,
-                                  // ),
-
-                                  template(
-                                    imageUrl: councilData.gensec.photo_url,
-                                    desg: 'gensec',
-                                    name: councilData.gensec.name,
-                                  ),
-
-                                  // template(
-                                  //   imageUrl:
-                                  //       councilData.joint_gensec[1].photo_url,
-                                  //   desg: 'joint gensec',
-                                  //   name: councilData.joint_gensec[1].name,
-                                  // ),
-                                ],
-                              ),
-                            ),
-                      Container(
-                        color: Colors.white,
-                        padding: EdgeInsets.all(5.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              'Clubs',
-                              style: headingStyle,
-                              textAlign: TextAlign.left,
-                            ),
-                            divide,
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                councilData == null
-                    ? Container(
-                        height: MediaQuery.of(context).size.height / 4,
-                        child: Center(
-                          child: CircularProgressIndicator(),
-                        ),
-                      )
-                    : Container(
-                        color: Colors.white,
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          physics: const NeverScrollableScrollPhysics(),
-                          itemCount: councilData.clubs.length,
-                          itemBuilder: (context, index) {
-                            return ListTile(
-                              onTap: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                    builder: (context) => ClubPage(
-                                      clubId: councilData.clubs[index].id,
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  Container(
+                    padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 0.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      //scrollDirection: Axis.vertical,
+                      children: <Widget>[
+                        space,
+                        Container(
+                          color: Colors.white,
+                          //margin: EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Container(
+                                color: Colors.white,
+                                padding: EdgeInsets.all(8.0),
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  children: [
+                                    Text(
+                                      'Description',
+                                      style: headingStyle,
                                     ),
-                                  ),
-                                );
-                              },
-                              leading: Container(
-                                height: 50.0,
-                                width: 50.0,
-                                decoration: BoxDecoration(
-                                    //color: Colors.black,
-                                    image: DecorationImage(
-                                      image: councilData.clubs[index]
-                                                  .small_image_url ==
-                                              null
-                                          ? AssetImage('assets/AMC.png')
-                                          : NetworkImage(councilData
-                                              .clubs[index].small_image_url),
-                                      fit: BoxFit.fill,
-                                    ),
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(30.0)),
-                                    border: Border.all(
-                                        color: Colors.blue, width: 2.0)),
-                              ),
-                              title: Container(
-                                child: Card(
-                                  shape: RoundedRectangleBorder(
-                                    side: BorderSide(
-                                        color: Colors.blue, width: 2.0),
-                                    borderRadius: BorderRadius.circular(15.0),
-                                  ),
-                                  color: Colors.black,
-                                  child: Container(
-                                    height: 50.0,
-                                    child: Center(
-                                      child: Text(councilData.clubs[index].name,
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 25.0)),
-                                    ),
-                                  ),
+                                    divide,
+                                    councilData == null
+                                        ? Center(
+                                            child: CircularProgressIndicator(),
+                                          )
+                                        : Text(councilData.description,
+                                            style: generalTextStyle),
+                                    space,
+                                  ],
                                 ),
                               ),
-                            );
-                          },
+                              councilData == null
+                                  ? Container(
+                                      height:
+                                          MediaQuery.of(context).size.height /
+                                              4,
+                                      child: Center(
+                                        child: CircularProgressIndicator(),
+                                      ),
+                                    )
+                                  : Container(
+                                      color: Colors.grey[300],
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        children: <Widget>[
+                                          // template(
+                                          //   imageUrl:
+                                          //       councilData.joint_gensec[0].photo_url,
+                                          //   desg: 'joint gensec',
+                                          //   name: councilData.joint_gensec[0].name,
+                                          // ),
+
+                                          template(
+                                            imageUrl:
+                                                councilData.gensec.photo_url,
+                                            desg: 'gensec',
+                                            name: councilData.gensec.name,
+                                          ),
+
+                                          // template(
+                                          //   imageUrl:
+                                          //       councilData.joint_gensec[1].photo_url,
+                                          //   desg: 'joint gensec',
+                                          //   name: councilData.joint_gensec[1].name,
+                                          // ),
+                                        ],
+                                      ),
+                                    ),
+                              Container(
+                                color: Colors.white,
+                                padding: EdgeInsets.all(5.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      'Clubs',
+                                      style: headingStyle,
+                                      textAlign: TextAlign.left,
+                                    ),
+                                    divide,
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-              ],
+                        councilData == null
+                            ? Container(
+                                height: MediaQuery.of(context).size.height / 4,
+                                child: Center(
+                                  child: CircularProgressIndicator(),
+                                ),
+                              )
+                            : Container(
+                                color: Colors.white,
+                                child: ListView.builder(
+                                  shrinkWrap: true,
+                                  physics: const NeverScrollableScrollPhysics(),
+                                  itemCount: councilData.clubs.length,
+                                  itemBuilder: (context, index) {
+                                    return ListTile(
+                                      onTap: () {
+                                        Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                            builder: (context) => ClubPage(
+                                              clubId:
+                                                  councilData.clubs[index].id,
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                      leading: Container(
+                                        height: 50.0,
+                                        width: 50.0,
+                                        decoration: BoxDecoration(
+                                            //color: Colors.black,
+                                            image: DecorationImage(
+                                              image: councilData.clubs[index]
+                                                          .small_image_url ==
+                                                      null
+                                                  ? AssetImage('assets/AMC.png')
+                                                  : NetworkImage(councilData
+                                                      .clubs[index]
+                                                      .small_image_url),
+                                              fit: BoxFit.fill,
+                                            ),
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(30.0)),
+                                            border: Border.all(
+                                                color: Colors.blue,
+                                                width: 2.0)),
+                                      ),
+                                      title: Container(
+                                        child: Card(
+                                          shape: RoundedRectangleBorder(
+                                            side: BorderSide(
+                                                color: Colors.blue, width: 2.0),
+                                            borderRadius:
+                                                BorderRadius.circular(15.0),
+                                          ),
+                                          color: Colors.black,
+                                          child: Container(
+                                            height: 50.0,
+                                            child: Center(
+                                              child: Text(
+                                                  councilData.clubs[index].name,
+                                                  style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontSize: 25.0)),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
