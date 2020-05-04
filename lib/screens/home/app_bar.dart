a7840278b48d4c3db34a6a528b9f816c58db972d
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-05-04 16:21:55 +0530

    home app bar fixed

--- lib/screens/home/app_bar.dart
+++ lib/screens/home/app_bar.dart
@@ -7,43 +7,45 @@ AppBar homeAppBar(context, {SearchBarWidget searchBarWidget}) => AppBar(
       automaticallyImplyLeading: false,
       elevation: 0,
       actions: <Widget>[
-        Container(
-          width: MediaQuery.of(context).size.width,
-          padding: EdgeInsets.fromLTRB(15, 8, 15, 0),
-          child: Card(
-            elevation: 5.0,
-            color: Colors.blue[200],
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(left: 5, right: 10),
-                  height: 35.0,
-                  width: 35.0,
-                  child: Builder(
-                    builder: (context) => GestureDetector(
-                        onTap: () => Scaffold.of(context).openDrawer()),
-                  ),
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AppConstants.currentUser == null
-                              ? AssetImage('assets/profile_test.jpg')
-                              : NetworkImage(AppConstants.currentUser.photoUrl),
-                          fit: BoxFit.cover),
-                      borderRadius: BorderRadius.circular(50.0)),
-                ),
-                Expanded(child: searchBarWidget.getSearchTextFeild(context)),
-                Padding(
-                  padding: const EdgeInsets.only(right: 8.0, left: 8),
-                  child: Container(
-                    child: InkWell(
-                      child:
-                          Icon(Icons.notifications_active, color: Colors.black),
-                      onTap: () {},
+        Flexible(
+          child: Container(
+            padding: EdgeInsets.fromLTRB(15, 8, 15, 0),
+            child: Card(
+              elevation: 5.0,
+              color: Colors.blue[200],
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(left: 5, right: 10),
+                    height: 35.0,
+                    width: 35.0,
+                    child: Builder(
+                      builder: (context) => GestureDetector(
+                          onTap: () => Scaffold.of(context).openDrawer()),
                     ),
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AppConstants.currentUser == null
+                                ? AssetImage('assets/profile_test.jpg')
+                                : NetworkImage(
+                                    AppConstants.currentUser.photoUrl),
+                            fit: BoxFit.cover),
+                        borderRadius: BorderRadius.circular(50.0)),
                   ),
-                )
-              ],
+                  Expanded(child: searchBarWidget.getSearchTextFeild(context)),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0, left: 8),
+                    child: Container(
+                      child: InkWell(
+                        child: Icon(Icons.notifications_active,
+                            color: Colors.black),
+                        onTap: () {},
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
