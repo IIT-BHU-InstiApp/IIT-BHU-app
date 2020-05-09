ae129f1c1f8b445240b9591bf159033fb7de8d5e
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-05-09 18:51:05 +0530

    added hostel placemarks at map

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -49,12 +49,34 @@ class TheMap extends StatefulWidget {
 class _MyAppState extends State<TheMap> {
   Color color = Colors.green;
   GoogleMapController mapController;
+  List<Marker> _markers = <Marker>[];
 
   void _onMapCreated(GoogleMapController controller) {
-    setState(() {
-      mapController = controller;
-      color = Colors.blue;
-    });
+    mapController = controller;
+    color = Colors.blue;
+    int i = 0;
+    for (var coord in coords['Hostels']) {
+      i += 1;
+      this._markers.add(Marker(
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+          markerId: MarkerId('Hostels ${i.toString()}'),
+          position: coord['LatLng'],
+          infoWindow: InfoWindow(
+            title: coord['title'],
+          ),
+          onTap: () {
+            mapController
+                .animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+              target: coords['LatLng'],
+              zoom: 18,
+              tilt: 75,
+              bearing: 45,
+            )));
+          }));
+    }
+
+    debugPrint(this._markers.toString());
+    setState(() {});
   }
 
   @override
@@ -69,20 +91,159 @@ class _MyAppState extends State<TheMap> {
           target: const LatLng(25.267878, 82.990494),
           zoom: 15,
           bearing: 0.0,
-          tilt: 45.0,
+          tilt: 0.0,
         ),
         mapType: MapType.terrain,
-        // MapType.hybrid,
-        // myLocationEnabled: true,
         indoorViewEnabled: true,
         mapToolbarEnabled: true,
-        cameraTargetBounds: CameraTargetBounds(LatLngBounds(
-            southwest: LatLng(25.259598295586617, 82.98727926886082),
-            northeast: LatLng(25.269851359020624, 82.99818585552365))),
-        minMaxZoomPreference: MinMaxZoomPreference(15, 25),
+        // minMaxZoomPreference: MinMaxZoomPreference(10, 25),
+        markers: Set.from(_markers),
         onTap: (tappedPosition) {
           print(
               'lattitude : ${tappedPosition.latitude}      longitude: ${tappedPosition.longitude}');
         },
       );
 }
+
+Map<String, dynamic> coords = {
+  "Hostels": [
+    {
+      "title": "Limbdi Hostel",
+      "LatLng": LatLng(25.261155, 82.986075),
+    },
+    {
+      "title": "Rajputana Hostel",
+      "LatLng": LatLng(25.262417, 82.986178),
+    },
+    {
+      "title": "Dhanraj Giri Hostel",
+      "LatLng": LatLng(25.263944, 82.986100),
+    },
+    {
+      "title": "Morvi Hostel",
+      "LatLng": LatLng(25.265029, 82.986136),
+    },
+    {
+      "title": "CV Raman Hostel",
+      "LatLng": LatLng(25.265925, 82.986291),
+    },
+    {
+      "title": "Ramanujan Hostel",
+      "LatLng": LatLng(25.263271, 82.984804),
+    },
+    {
+      "title": "Aryabhatta Hostel",
+      "LatLng": LatLng(25.264109, 82.984266),
+    },
+    {
+      "title": "S.N. Bose Hostel",
+      "LatLng": LatLng(25.263361, 82.984020),
+    },
+    {
+      "title": "Visvesvaraya Hostel",
+      "LatLng": LatLng(25.262324, 82.984066),
+    },
+    {
+      "title": "S.C Dey Hostel",
+      "LatLng": LatLng(25.260016, 82.986522),
+    },
+    {
+      "title": "Vivekananda Hostel",
+      "LatLng": LatLng(25.259132, 82.986793),
+    },
+    {
+      "title": "Vishwakarma Hostel",
+      "LatLng": LatLng(25.257850, 82.985653),
+    },
+    {
+      "title": "GSC(old) Hostel",
+      "LatLng": LatLng(25.260617, 82.984292),
+    },
+    {
+      "title": "GSC(ext) Hostel",
+      "LatLng": LatLng(25.260247, 82.984530),
+    },
+    {
+      "title": "New Girls Hostel",
+      "LatLng": LatLng(25.261197, 82.983797),
+    },
+  ],
+  "Departments": [
+    {
+      "title": "Electronics",
+      "LatLng": LatLng(25.262838, 82.990496),
+    },
+    {
+      "title": "CSE",
+      "LatLng": LatLng(25.262479, 82.993714),
+    },
+    {
+      "title": "MnC",
+      "LatLng": LatLng(25.261849, 82.993503),
+    },
+    {
+      "title": "Electrical",
+      "LatLng": LatLng(25.261384, 82.992030),
+    },
+    {
+      "title": "Mechanical",
+      "LatLng": LatLng(25.261692, 82.991760),
+    },
+    {
+      "title": "Civil",
+      "LatLng": LatLng(25.262817, 82.991948),
+    },
+    {
+      "title": "Ceramic",
+      "LatLng": LatLng(25.259739, 82.992809),
+    },
+    {
+      "title": "Engineering Physics",
+      "LatLng": LatLng(25.259533, 82.992948),
+    },
+  ],
+  "Others": [
+    {
+      "title": "Main library ",
+      "LatLng": LatLng(25.261830, 82.989142),
+    },
+    {
+      "title": "Technex office ",
+      "LatLng": LatLng(25.261365, 82.986336),
+    },
+    {
+      "title": "CCD ",
+      "LatLng": LatLng(25.258250, 82.986543),
+    },
+    {
+      "title": "GTAC ",
+      "LatLng": LatLng(25.259733, 82.984981),
+    },
+    {
+      "title": "Cafeteria ",
+      "LatLng": LatLng(25.260441, 82.991436),
+    },
+    {
+      "title": "Rajputana ground ",
+      "LatLng": LatLng(25.262233, 82.987315),
+    },
+    {
+      "title": "Gymkhana ",
+      "LatLng": LatLng(25.259643, 82.988997),
+    },
+    {
+      "title": "ADV ",
+      "LatLng": LatLng(25.258719, 82.990153),
+    },
+  ],
+  "Lecture Halls": [
+    {
+      "title": "ABLT ",
+      "LatLng": LatLng(25.262779, 82.988583),
+    },
+    {
+      "title": "LT3 ",
+      "LatLng": LatLng(25.258845, 82.992690),
+    },
+  ],
+};
