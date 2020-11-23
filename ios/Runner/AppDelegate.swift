d099f37d0dc1789ca200e55a3f4627b46a98ba91
captain26 <56953124+captain26@users.noreply.github.com>
2020-11-23 13:21:41 +0530

    Google Maps API Added to iOS (#240)
    * GoogleAuth for iOS
    
    * Google API Added to iOS
    
    Co-authored-by: vasu bansal <vinidhanuka@gmail.com>

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyCXlRxfbr9Y368nLy8o59r0_XZmHdK5-2w")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
