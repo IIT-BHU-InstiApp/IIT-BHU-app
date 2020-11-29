bf8f52f4de80afcd07c79570f98944ae401cf78b
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-11-29 10:21:46 +0530

    flushbar functionality

--- lib/ui/internet_error_flushbar.dart
+++ lib/ui/internet_error_flushbar.dart
@@ -1,6 +1,34 @@
 import 'package:flushbar/flushbar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class InternetErrorFlushbar {
   Flushbar flushbar;
   bool onScreen;
+
+  InternetErrorFlushbar() {
+    onScreen = false;
+    flushbar = Flushbar(
+      title: 'Internet Error',
+      message: 'Could not connect to internet, please check your connection.',
+      duration: Duration(seconds: 2),
+      flushbarPosition: FlushbarPosition.BOTTOM,
+      flushbarStyle: FlushbarStyle.FLOATING,
+      showProgressIndicator: true,
+      progressIndicatorBackgroundColor: Colors.blueGrey,
+      isDismissible: false,
+      icon: Icon(Icons.error_outline, color: Colors.red),
+    );
+    flushbar.onStatusChanged = _handleStatus;
+  }
+
+  void _handleStatus(FlushbarStatus status) {
+    switch (status) {
+      case FlushbarStatus.SHOWING:
+        onScreen = true;
+        break;
+      default:
+        onScreen = false;
+    }
+  }
 }
